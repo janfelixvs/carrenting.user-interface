1243a2c986d6d66a4e61d8b8800630ab67555a46
Drolt <thomas_klug@gmx.de>
2024-01-04 11:31:40 +0100

    welcome script added

---
+++ customerSiteRelease/welcome/scripts.js
@@ -0,0 +1,8 @@
+function login() {
+    // Redirect to the desired logout URL
+    window.location.href = '../logged/logged.html'; // Replace with the path to your logout page
+}
+
+function signUp() {
+    
+}
